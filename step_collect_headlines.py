21f96d2a3895329d7d467e298bdce10c6f30297e
Mateusz Cieslak <mateusz.cieslak@gmail.com>
2022-05-19 18:18:06 +0200

    Removed non-default parameters for object inits

--- step_collect_headlines.py
+++ step_collect_headlines.py
@@ -4,11 +4,11 @@ import constants
 
 
 def run():
-    ns = news_sources.NewsSources(api_url=constants.SOURCES_URL, physical_path=constants.NEWS_SOURCES_PATH, api_key=constants.API_KEY)
+    ns = news_sources.NewsSources()
     ns.read()
     sources = ns.list_sources()
 
-    h = headlines.Headlines(api_url=constants.HEADLINES_URL, physical_path=constants.HEADLINES_PATH, api_key=constants.API_KEY)
+    h = headlines.Headlines()
     h.collect(news_sources=sources, page_size=constants.RESULTS_PER_PAGE)
     h.save()
 
